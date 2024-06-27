d2f6984c74bc434f42432d087c55dc43cb3e6dd3
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-06-27 12:20:26 -0300

    cart and item tile implementation

--- components/ItemTile.tsx
+++ components/ItemTile.tsx
@@ -1,11 +1,56 @@
 'use client'
 import React from 'react';
+import Image from 'next/image'
+import { formatNumber } from '@/public/utils/formatNumber';
+import { Add, Remove } from '@blueprintjs/icons'
+import { useCart } from '@/contexts/CartContext';
 
-type ItemTileProps = {};
+interface ItemTileProps {
+    product: {
+        id: number;
+        name: string;
+        price: number;
+        amount: number;
+        image: string;
+    };
+}
 
-const ItemTile: React.FC<ItemTileProps> = () => {
+const ItemTile: React.FC<ItemTileProps> = ({ product }) => {
+
+    const { cart, addToCart, removeFromCart } = useCart();
+
     return (
-        <div>ItemTile</div>
+        <div key={product.id} className='flex p-4 bg-white justify-between rounded-xl text-black'>
+            <div className='flex flex-col gap-3 justify-between'>
+                <p className='text-lg w-2/3 font-semibold'>
+                    {product.name}
+                </p>
+                <p className='text-md bg-zinc-700 text-white rounded-md p-1 pl-2 pr-2 font-bold self-start'>
+                    R$ {formatNumber(product.price.toString())}
+                </p>
+            </div>
+            <div className='flex flex-col items-center justify-between p-5 gap-3'>
+                <Image
+                    src={product.image ? product.image : '/assets/images/missing_image.png'}
+                    alt={product.name}
+                    width={65}
+                    height={65}
+                />
+                <div className='flex gap-2 justify-center items-center text-black cursor-pointer'>
+                    <Add
+                        color='green'
+                        onClick={() => {addToCart(product)}}
+                    />
+                    <p className='text-lg font-semibold'>
+                        {product.amount}
+                    </p>
+                    <Remove
+                        color='red'
+                        onClick={() => { removeFromCart(product.id) }}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
 
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import { ShoppingCart } from '@blueprintjs/icons';
 import { Drawer } from '@mui/material';
+import { useCart } from '@/contexts/CartContext';
+import { colors } from '@/styles/colors';
+import ItemTile from './ItemTile';
+import { formatNumber } from '@/public/utils/formatNumber';
+import { Delete, Trash } from '@blueprintjs/icons'
 
 
 type NavProps = {};
@@ -10,9 +15,10 @@ const Nav: React.FC<NavProps> = () => {
 
     const [cartOpen, setCartOpen] = React.useState<boolean>(false);
 
+    const { cartCount, totalAmount, cart, clearCart } = useCart();
 
     return (
-        <div className='w-full bg-[#0f52ba] flex justify-between p-4'>
+        <div className='w-full bg-primary flex justify-between p-4'>
             <div className='flex justify-center items-center gap-2 ml-6'>
                 <h1 className='text-white text-2xl font-medium'>
                     MKS
@@ -28,7 +34,7 @@ const Nav: React.FC<NavProps> = () => {
                 >
                     <ShoppingCart color='black' />
                     <p className='font-bold'>
-                        {/* {totalItems} */}
+                        {cartCount}
                     </p>
                 </button>
             </div>
@@ -37,22 +43,41 @@ const Nav: React.FC<NavProps> = () => {
                 anchor='right'
                 open={cartOpen}
                 onClose={() => {setCartOpen(false)}}
-                sx={{ '& .MuiDrawer-paper': { width: '30rem', backgroundColor: '#0f52ba' } }}
+                sx={{ '& .MuiDrawer-paper': { width: '30rem', backgroundColor: colors.primary } }}
             >
-                <div className='flex flex-nowrap items-center justify-between p-8'>
-                    <h1 className='text-white text-3xl font-semibold'>
-                        Carrinho de compras
-                    </h1>
-                    <button 
-                        className='bg-black text-white' 
-                        style={{ width: '35px', height: '35px', borderRadius: '50%' }}
-                        onClick={() => {setCartOpen(false)}}
-                    >
-                        X
-                    </button>
+                <div className='flex flex-col'>
+                    <div className='flex flex-nowrap items-center justify-between p-8'>
+                        <h1 className='text-white text-3xl font-semibold'>
+                            Carrinho de compras
+                        </h1>
+                        <button
+                            onClick={() => {setCartOpen(false)}}
+                        >
+                            <Delete
+                                color='white'
+                                size={20}
+                            />
+                        </button>
+                    </div>
+                    <div className='flex items-center w-full font-semibold p-4 pl-8 pr-8 pt-0  text-white'>
+                        <button 
+                            className='flex items-center justify-center gap-2 cursor-pointer rounded-lg p-1 hover:bg-white hover:text-black transition duration-100 ease-in-out'
+                            onClick={() => { clearCart() }}    
+                        >
+                            <div>
+                                <Trash
+                                    color='red'
+                                    size={16}
+                                />
+                            </div>
+                            Clear Cart
+                        </button>
+                    </div>
                 </div>
-                <div className='p-8 pt-0'>
-
+                <div className=' flex flex-col gap-3 p-8 pt-0'>
+                    {cart.map((product) => (
+                        <ItemTile key={product.id} product={product} />
+                    ))}
                 </div>
                 <div className='mt-auto'>
                     <div className='text-2xl text-white font-semibold flex justify-between p-8 pl-12 pr-12'>
@@ -60,7 +85,7 @@ const Nav: React.FC<NavProps> = () => {
                             Total:
                         </p>
                         <p>
-                            R$0
+                            R$ {formatNumber(totalAmount.toString())}
                         </p>
                     </div>
                     <button className='bg-black text-white text-2xl font-semibold w-full p-6'>
