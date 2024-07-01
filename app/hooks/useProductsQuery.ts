4bd356689145897d5531da24e1d169f79b8802f4
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-07-01 18:14:15 -0300

    code refactoring + category search working

--- app/hooks/useProductsQuery.ts
+++ app/hooks/useProductsQuery.ts
@@ -1,12 +1,26 @@
 
 
 
-const fetchProducts = async (limit: number): Promise<any> => {
-    const response = await fetch(`https://fakestoreapi.com/products?limit=${limit}`);
+export const fetchProducts = async (): Promise<any> => {
+    const response = await fetch(`https://fakestoreapi.com/products`);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
     }
     return response.json();
 };
 
-export default fetchProducts;
+export const fetchCategories = async (): Promise<any> => {
+    const response = await fetch(`https://fakestoreapi.com/products/categories`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch categories');
+    }
+    return response.json();
+};
+
+export const fetchByCategory = async (category: string): Promise<any> => {
+    const response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch categories');
+    }
+    return response.json();
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import fetchProducts from './hooks/useProductsQuery';
+import { fetchProducts, fetchCategories, fetchByCategory } from './hooks/useProductsQuery';
 import ItemCard from '@/components/ItemCard';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 export default function Home() {
   
-  const limit = 7;
-
   interface Product {
     id: number;
     title: string;
@@ -16,37 +15,98 @@ export default function Home() {
     price: string;
     image: string;
   }
-
+  
   const [data, setData] = React.useState<Product[]>([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
+  const [categories, setCategories] = React.useState<string[]>([]);
+
+  const getAllProducts = async () => {
+    try {
+      const data = await fetchProducts();
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const getCategories = async () => {
+    try {
+      const data = await fetchCategories();
+      setCategories(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getProductsByCategory = async (category: string) => {
+    setIsLoading(true);
+    try {
+      if (category === 'all') {
+        await getAllProducts();
+      } else {
+        const data = await fetchByCategory(category);
+        setData(data);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   useEffect(() => {
-    fetchProducts(limit)
-      .then(data => {setData(data); setIsLoading(false);})
-      .catch(error => console.error(error));
+    getAllProducts();
+    getCategories();
   }, []);
 
+  const handleSearchChange = (category: string) => {
+    getProductsByCategory(category);
+  }
   
   return (
-    <main className="w-full flex justify-center">
-      {isLoading ? (
-        <div className='w-full flex justify-center pt-40'>
-          <LoadingSpinner loading={isLoading} />
+    <main className="w-full">
+        <div className='flex flex-nowrap'>
+          <div className='w-1/6 ml-8'>
+            <FormControl fullWidth>
+              <InputLabel>Categorias</InputLabel>
+              <Select
+                label="Categorias"
+                defaultValue={'all'}
+                onChange={(e) => handleSearchChange(e.target.value)}
+              >
+                <MenuItem key={'all'} value={'all'}>Todas</MenuItem>
+                {
+                  categories.map(category => (
+                    <MenuItem key={category} value={category}>{category}</MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>
+          </div>
+          <div className='w-full flex justify-center'>
+            {isLoading ? (
+              <div className='w-full flex justify-center pt-52'>
+                <LoadingSpinner loading={isLoading} />
+              </div>
+            ) : (
+            <div className='grid sm:grid-cols-2 xl:grid-cols-4 gap-6'>
+              {data?.map(product => (
+                <ItemCard
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  description={product.description}
+                  price={product.price}
+                  image={product.image}
+                />
+              ))}
+            </div>
+            )}
+          </div>
         </div>
-      ) : (
-        <div className='grid sm:grid-cols-2 xl:grid-cols-4 gap-6'>
-          {data?.map(product => (
-            <ItemCard
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-              image={product.image}
-            />
-          ))}
-        </div>
-      )}
     </main>
   );
 }
